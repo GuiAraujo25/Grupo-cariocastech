6cafbf842ea40bb8edef8f2d9b234a6873c56da6
joaocellbit <joaomarciopradosilva@gmail.com>
2024-11-21 08:40:51 -0300

    arrumando projetos

--- react-base/src/pages/Projetos.jsx
+++ react-base/src/pages/Projetos.jsx
@@ -2,6 +2,7 @@ import Cartao from "../components/Cartao/Cartao";
 import ListContainer from "../components/ListContainer/ListContainer";
 import Base from "./Base";
 import dados_projetos from "../data/projetos.json"
+import ProjetoCartoes from "../components/ProjetoCartoes/ProjetoCartoes";
 
 const dados = dados_projetos
 
@@ -9,11 +10,16 @@ const Projetos = () => (
     <Base>
        <ListContainer>
        {
-           dados.map( (ele, i) => (
+           dados.map( (item, i) => (
                    <Cartao
-                       key={i}
-                       titulo={ele.nome_projeto}
-                       texto={ele.titulo}
+                    key={item.id} // Agora usa o id gerado dinamicamente
+                    titulo={item.titulo}
+                    texto={item.texto}
+                    imagem={item.imagem}
+                    botao={item.botao}
+                    id={item.id} // Passa o id para a página de detalhes
+                    showBotao={true}
+                    showDescricao={true}
                     />
            ))
        }
